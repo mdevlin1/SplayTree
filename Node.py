5bf638fd390c88879162799a27ee127d12a0051a
Mitchell Devlin <mitchell.r.devlin@gmail.com>
2021-02-28 20:51:26 -0500

    WIP Starting to clean up and organize this implementation of a splay tree

---
+++ Node.py
@@ -0,0 +1,6 @@
+class Node:
+    def __init__(self, value, parent):
+        self.value = value
+        self.parent = parent
+        self.left = None
+        self.right = None 
---
+++ main.py
@@ -0,0 +1,9 @@
+from SplayTree import Splay
+
+def main():
+    tree = Splay()
+    tree.insert(6)
+    tree.insert(10)
+
+if __name__ == '__main__':
+    main()
