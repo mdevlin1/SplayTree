53ff34da8963e1af449f978f21dc59ac4c4308e7
Mitchell Devlin <mitchell.r.devlin@gmail.com>
2021-07-05 19:41:15 -0400

    Refactored my Splay Tree implementation. I created a base Binary Tree class that encompasses all of the binary tree specific logic of splay trees

--- Node.py
+++ Node.py
@@ -1,3 +1,11 @@
+from enum import Enum
+
+# Enum that indicates if a node is to the
+# left or the right of it's parent
+class Position(Enum):
+    LEFT = 1,
+    RIGHT = 2
+
 class Node:
     def __init__(self, value, parent):
         self.value = value
@@ -5,4 +13,10 @@ class Node:
         self.left = None
         self.right = None 
 
-    def swapParent():
+    def findPosition(self):
+        if self.value > self.parent.value:
+            return Position.RIGHT
+        elif self.value < self.parent.value:
+            return Position.LEFT
+        else:
+            raise Exception('Undefined behavior, values do not fit binary tree definition')
--- SplayTree.py
+++ SplayTree.py
@@ -1,42 +1,7 @@
 from Node import Node
+from BinaryTree import BinaryTree
 
-class Splay:
-    def __init__(self):
-        self.root = None
-
-    def insert(self, value):
-        if self.root is None:
-            self.root = Node(value, None)
-
-        elif value > self.root.value:
-            if self.root.right is None:
-                self.root.right = Node(value, self.root)
-                self.splayUp(self.root.right)
-            else:
-                self.insertRecursive(self.root.right, self.root, value)
-
-        else:
-            if self.root.left is None:
-                self.root.left = Node(value, self.root)
-                self.splayUp(self.root.left)
-            else:
-                self.insertRecursive(self.root.left, self.root, value)    
-
-    def insertRecursive(self, node, parent, value):
-        if value > node.value:
-            if node.right is None:
-                node.right = Node(value, node)
-                self.splayUp(node.right)
-            else:
-                self.insertRecursive(node.right, node, value)
-
-        else:
-            if node.left is None:
-                node.left = Node(value, node)
-                self.splayUp(node.left)
-            else:
-                self.insertRecursive(node.left, node, value)
-
+class Splay(BinaryTree):
 
     def zig(self, parent, node):
         if node.right is None and node.left is None:
